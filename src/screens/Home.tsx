2a48da2c7d2da6c972387d692ad3c2770c6176ef
F6075077 Kenard Rodrigues da Silva <kenardrs@gmail.com>
2024-07-22 09:50:40 -0300

    groups 2 - creating groups list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,26 +1,31 @@
 import { useState } from 'react'
-import { HStack, Text, VStack } from 'native-base'
+import { VStack, FlatList } from 'native-base'
 
 import { HomeHeader } from '@components/HomeHeader'
 import { Group } from '@components/Group'
 
 export const Home = () => {
-    const [groupSelected, setGroupSelected] = useState('costa')
+    const [groups, setGroups] = useState(['costas', 'bíceps', 'tríceps', 'ombro'])
+    const [groupSelected, setGroupSelected] = useState('costas')
 
     return (
         <VStack flex={1}>
             <HomeHeader title="Kenard Silva" subTitle='Olá,'/>
-            <HStack>
-                <Group name="costa" 
-                    isActive={groupSelected === 'costa'} 
-                    onPress={() => setGroupSelected('costa')}
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Group name={item}
+                        isActive={groupSelected === item} 
+                        onPress={() => setGroupSelected(item)}
                     />
-                <Group 
-                    name="ombro" 
-                    isActive={groupSelected === 'ombro'} 
-                    onPress={() => setGroupSelected('ombro')}
-                />
-            </HStack>
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 8 }}
+                my={10}
+                maxH={10}
+            />
         </VStack>
     )
 }
