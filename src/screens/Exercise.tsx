00336f0b428278aa13098f073bdf7aac8c86072b
F6075077 Kenard Rodrigues da Silva <kenardrs@gmail.com>
2024-07-24 13:01:50 -0300

    exercise screen - buildong header details

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -2,10 +2,12 @@ import { TouchableOpacity } from 'react-native'
 
 import { useNavigation } from '@react-navigation/native'
 
-import { VStack, Icon } from 'native-base'
+import { VStack, Icon, HStack, Heading, Text } from 'native-base'
 import { Feather } from '@expo/vector-icons'
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
+import BodySvg from '@assets/body.svg'
 
 export const Exercise = () => {
 
@@ -21,6 +23,21 @@ export const Exercise = () => {
                 <TouchableOpacity onPress={handleGoBack}>
                     <Icon as={Feather} name='arrow-left' color={'green.500'} size={6} />
                 </TouchableOpacity>
+
+                <HStack justifyContent={'space-between'} mt={4} mb={8} alignItems={'center'}>
+                    <Heading color={'gray.100'} fontSize={'lg'} flexShrink={1}>
+                        Puxada frontal
+                    </Heading>
+
+                    <HStack alignItems={'center'}>
+                        <BodySvg />
+
+                        <Text color={'gray.200'} ml={1} textTransform={'capitalize'}>
+                            Costas
+                        </Text>
+                    </HStack>
+
+                </HStack>
             </VStack>
         
         </VStack>
