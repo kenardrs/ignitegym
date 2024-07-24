8919004161d815352a75d433db57a406ac3947d9
F6075077 Kenard Rodrigues da Silva <kenardrs@gmail.com>
2024-07-24 12:38:19 -0300

    exercise screen - creating goback button

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,28 @@
-import { Center, Text } from 'native-base'
+import { TouchableOpacity } from 'react-native'
+
+import { useNavigation } from '@react-navigation/native'
+
+import { VStack, Icon } from 'native-base'
+import { Feather } from '@expo/vector-icons'
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
 export const Exercise = () => {
+
+    const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+    const handleGoBack = () => {
+        navigation.goBack()
+    } 
+
     return (
-        <Center flex={1}>
-            <Text color="white">Exercise</Text>
-        </Center>
+        <VStack flex={1}>
+            <VStack px={8} bg={'blueGray.600'} pt={12}>
+                <TouchableOpacity onPress={handleGoBack}>
+                    <Icon as={Feather} name='arrow-left' color={'green.500'} size={6} />
+                </TouchableOpacity>
+            </VStack>
+        
+        </VStack>
     )
 }
