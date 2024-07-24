fea74221a8c08f209528833b65063389a93b61f5
F6075077 Kenard Rodrigues da Silva <kenardrs@gmail.com>
2024-07-24 11:10:46 -0300

    profile screen done

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,83 @@
-import { Center, Text } from 'native-base'
+import { TouchableOpacity } from 'react-native'
+import { VStack, ScrollView, Center, Skeleton, Text, Heading} from 'native-base'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+import { useState } from 'react'
+import { Input } from '@components/Input'
+import { Button } from '@components/Button'
+
+const PHOTO_SIZE = 33
 
 export const Profile = () => {
+    const [loadingPhoto, setLoadingPhoto] = useState(false)
+
     return (
-        <Center flex={1}>
-            <Text color="white">Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title='Perfil'/>
+
+            <ScrollView>
+                <Center mt={6} px={10}>
+                    {
+                        loadingPhoto
+                            ? <Skeleton 
+                                w={PHOTO_SIZE} 
+                                h={PHOTO_SIZE}
+                                rounded={'full'}
+                                startColor={'gray.500'}
+                                endColor={'gray.400'}
+                            />
+                            : <UserPhoto
+                                source={{ uri: 'https://github.com/kenardrs.png' }}
+                                alt='Foto do Usuário'
+                                size={PHOTO_SIZE} 
+                            />
+                    }
+                    <TouchableOpacity>
+                        <Text color={'green.500'} fontWeight={'bold'} fontSize={'md'} mt={2} mb={8}>
+                            Alterar foto
+                        </Text>
+                    </TouchableOpacity>
+
+                    <Input 
+                        bg={'gray.600'}
+                        placeholder='Nome'
+                    />
+                    <Input 
+                        bg={'gray.600'}
+                        placeholder='E-mail'
+                        isDisabled
+                    />
+
+                </Center>
+                
+                <VStack px={10} mt={12} mb={9}>
+                    <Heading color={'gray.200'} fontSize={'md'} mb={2}>
+                        Alterar senha
+                    </Heading>
+                
+                    <Input 
+                        bg={'gray.600'}
+                        placeholder='Senha antiga'
+                        secureTextEntry
+                    />
+                    <Input 
+                        bg={'gray.600'}
+                        placeholder='Nova senha'
+                        secureTextEntry
+                    />
+                    <Input 
+                        bg={'gray.600'}
+                        placeholder='Confirme a nova senha'
+                        secureTextEntry
+                    />
+
+                    <Button 
+                        mt={4}
+                        title='Atualizar'
+                    />
+                </VStack>
+            </ScrollView>
+            
+        </VStack>
     )
 }
